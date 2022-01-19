20968c224ae2757c3498a099aeb1337002562f96
Nada Hasni <nadasthing@gmail.com>
2022-01-19 15:24:41 +0700

    next js basic routing

---
+++ pages/users/detail.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function UserDetail() {
+  return <div>
+      <h1>User Detail</h1>
+  </div>;
+}
---
+++ pages/users/index.tsx
@@ -0,0 +1,5 @@
+export default function Users() {
+  return <div>
+      <h1>List Users</h1>
+  </div>;
+}
