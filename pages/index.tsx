e25752a40d267d4b37a19d6e84e75556cacb975f
Nada Hasni <nadasthing@gmail.com>
2022-01-20 13:56:46 +0700

    Image optimization using Image Component Next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout pageTitle="Home Page">
+      <Image src="/profile.jpg" width={200} height={200} alt="Profile" />
       <h1>Home</h1>
     </Layout>
   );
