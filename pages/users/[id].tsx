a89e8c2f6db9fe2f43723f378c34ccfe2804344e
Nada Hasni <nadasthing@gmail.com>
2022-01-20 14:00:37 +0700

    dynamic route using useRouter

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,10 +1,18 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 import Layout from '../../components/Layout';
 
 export default function UserDetail() {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <Layout pageTitle="Users Page">
-      <h1>User Detail</h1>
+      <h1>
+        User Detail
+        {' '}
+        {id}
+      </h1>
     </Layout>
   );
 }
