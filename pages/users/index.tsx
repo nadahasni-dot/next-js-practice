3549cd004e95d0c567b70621f61b32bf99cd5c66
Nada Hasni <nadasthing@gmail.com>
2022-01-20 14:17:10 +0700

    data fetching getStaticProps

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,34 @@
 import Layout from '../../components/Layout';
 
-export default function Users() {
+interface UsersProps {
+  dataUsers: Array<any>;
+}
+
+export default function Users(props: UsersProps) {
+  const { dataUsers } = props;
+
   return (
     <Layout pageTitle="User Detail Page">
       <h1>List Users</h1>
+      <ul>
+        {dataUsers.map((user) => (
+          <li>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+
+  return {
+    props: {
+      dataUsers,
+    },
+  };
 }
